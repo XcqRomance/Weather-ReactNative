4b3f622264d9c7c93c6557580422021fe2ad416b
XcqRomance <755412806@qq.com>
2017-12-28 10:52:28 +0800

    icon和launch页使用原声添加完成

---
+++ DailyWeather/android/app/src/main/java/com/dailyweather/LaunchActivity.java
@@ -0,0 +1,31 @@
+package com.dailyweather;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LaunchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launch);
+
+//        Intent intent = new Intent(LaunchActivity.this, MainActivity.class);
+//        startActivity(intent);
+//        LaunchActivity.this.finish();
+
+        Integer time = 500;    //设置等待时间，单位为毫秒
+        Handler handler = new Handler();
+        //当计时结束时，跳转至主界面
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(LaunchActivity.this, MainActivity.class));
+                LaunchActivity.this.finish();
+            }
+        }, time);
+
+    }
+}
